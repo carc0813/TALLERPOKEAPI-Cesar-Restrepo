ee1a1ec964cd4eac020df126a05b31330d6c1e79
carc0813 <carc1023@hotmail.com>
2026-07-07 08:37:31 -0500

    optimizacion del codigo

--- pokedex.js
+++ pokedex.js
@@ -3,7 +3,9 @@ async function pedirNombre() {
   //buscarPokemon(nombre);
   for (let i = 0; i < 3; i++) {
     let nombre = prompt("Escribe Nombre: ").toLowerCase();
-    await buscarPokemon(nombre);
+   const datos= await buscarPokemon(nombre);
+   mostrarFicha(datos);
+
   }
 }
 
@@ -16,41 +18,48 @@ async function buscarPokemon(nombre) {
     return null;
   }
   const datos = await respuesta.json();
-  console.log("Nombre:", datos.name.toUpperCase(), "ID:", datos.id);
+//  console.log("Nombre:", datos.name.toUpperCase(), "ID:", datos.id);
+  return datos;
 }
 
-async function mostrarFicha(datos) {
-  const respuesta = await fetch("https://pokeapi.co/api/v2/pokemon/");
+function mostrarFicha(datos) {
+
   if (!datos) {
-    console.log("No hay datos que Mostrar");
+    console.log("No hay datos que mostrar");
     return;
   }
 
-  const datos = respuesta.json();
-  console.log("Nobre:", datos.name.toUpperCase());
+  console.log("Nombre:", datos.name.toUpperCase());
   console.log("ID:", datos.id);
-  //tipos
+
+  // Tipos
   let tipos = [];
+
   for (let i = 0; i < datos.types.length; i++) {
     tipos.push(datos.types[i].type.name);
   }
-  console.log("Tipos:", tipos.join("/"));
 
-  //altura y peso
+  console.log("Tipos:", tipos.join(" / "));
+
+  // Altura y peso
   console.log("Altura:", datos.height * 10, "cm");
-  console.log("Peso:", datos.weight / 10, "Kg");
+  console.log("Peso:", datos.weight / 10, "kg");
 
-  //estadisticas
+  // Estadísticas
+  console.log("Estadísticas:");
   for (let i = 0; i < datos.stats.length; i++) {
-    console.log(datos.stats[i].name + ":" + datos.stats[i].base_stat);
+    console.log(datos.stats[i].stat.name + ": " + datos.stats[i].base_stat);
   }
-  //habilidades
-  console.log("Habilidades");
-  for (let i = 0; i < datos.abilities; i++) {
+
+  // Habilidades
+  console.log("Habilidades:");
+  for (let i = 0; i < datos.abilities.length; i++) {
     let habilidad = datos.abilities[i].ability.name;
+
     if (datos.abilities[i].is_hidden) {
-      habilidad += "oculta";
+      habilidad += " (oculta)";
     }
+
     console.log(habilidad);
   }
 }
